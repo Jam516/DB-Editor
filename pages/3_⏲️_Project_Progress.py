31a00a2a129dfade03fbd06b6ff0737b0419c6ee
Jam516 <kufuorkofi@gmail.com>
2024-07-15 00:09:57 +0100

    commit

--- pages/3_⏲️_Project_Progress.py
+++ pages/3_⏲️_Project_Progress.py
@@ -3,15 +3,15 @@ import json
 import streamlit as st
 import time
 
-st.set_page_config(layout="wide", page_title="Project Contracts", page_icon="🧮")
+st.set_page_config(layout="wide", page_title="Project Progress", page_icon="🧮")
 
 conn = st.connection("snowflake")
 
-st.title("Project Contracts Table Editor 🧮")
+st.title("Project Progress Table Editor 🧮")
 
 session = conn.session()
 
-df = session.table("ARBIGRANTS_LABELS_PROJECT_CONTRACTS").to_pandas()
+df = session.table("ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_MILESTONES").to_pandas()
 
 with st.form("data_editor_form"):
     st.markdown("**Edit the table and then click the UPDATE TABLE button to save your changes.**")
@@ -21,7 +21,7 @@ with st.form("data_editor_form"):
 if submit_button:
     try:
         #Note the quote_identifiers argument for case insensitivity
-        session.write_pandas(edited_df, "ARBIGRANTS_LABELS_PROJECT_CONTRACTS", overwrite=True, quote_identifiers=False)
+        session.write_pandas(edited_df, "ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_MILESTONES", overwrite=True, quote_identifiers=False)
         st.success("Table updated")
         time.sleep(5)
     except:
